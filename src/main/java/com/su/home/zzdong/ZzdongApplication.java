63968bd850e01c613709672ac1d88ab026a1fc49
jianzhong <978049618@qq.com>
2020-07-14 21:55:58 +0800

    初始提交

---
+++ src/main/java/com/su/home/zzdong/ZzdongApplication.java
@@ -0,0 +1,13 @@
+package com.su.home.zzdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzdongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/su/home/zzdong/ZzdongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.home.zzdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzdongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
